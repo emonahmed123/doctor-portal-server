c3cc1cce09030d06ee171ef2feba1e7b2c61bb3a
emonahmed123 <imonshomon@gmail.com>
2022-05-16 21:02:35 -0700

    added my token verifiytoken

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
 const port =process.env.PROT || 5000
@@ -15,11 +16,34 @@ const port =process.env.PROT || 5000
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6lyyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  console.log(authHeader)
+  const token = authHeader.split(' ')[1];
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+   console.log(decoded)
+   
+   if (err) {
+      return res.status(403).send({ message: 'Forbidden access' })
+    }
+  
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run(){
 try{
   await client.connect() ;
   const serviceCollection =client.db('doctor_portal').collection('services')
   const bookingCollection =client.db('doctor_portal').collection('bookings')
+  const userCollection =client.db('doctor_portal').collection('users')
 
 
      app.get('/service', async(req , res)=>{
@@ -28,14 +52,31 @@ try{
         const services = await cursor.toArray()
         res.send(services)
      });
-// this is not the proper way to query 
+        
+        app.put('/user/:email',async(req,res)=>{
+          const email = req.params.email;
+          const user = req.body
+          const filter = {email: email};
+          const options ={upsert: true};
+           const updateDoc ={
+             $set : user,
+           };
+           const result =await userCollection.updateOne(filter, updateDoc , options)
+            const token =jwt.sign({email:email}, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+           res.send({ result ,token});
+          });
+
+
+
+
+// this is not the proper way to query  after leatning more about mongodb use. aggregate ,lookup ,pipelind,match
    app.get('/available',async(req,res)=>{
      const date= req.query.date ;
  
      // step1: get aall services
      const services = await serviceCollection.find().toArray( )
       // step 2: get the booking of that day out[{},{},{} {} {}]
-      const query ={date: date};
+      const query ={date:date};
      const bookings = await bookingCollection.find(query).toArray()
       // step3 : for each service find bookings for that service.
       // services.forEach(service =>{
@@ -64,15 +105,25 @@ try{
   * app.get('/booking/:id')  // get a specitfic booking
   * app.post(booking) // add a new  booking
   * app.patch(/booking/:id)//
+  * app.put('booking/:id)// upsert  upadet ==> update (if exitst) or insert(if dosn,t exist)
   * app.delete('booking/:id)
   */
-  app.get('/booking', async(req, res) =>{
+
+  app.get('/booking', verifyJWT, async (req, res) => {
     const patient = req.query.patient;
-    const query = {patient: patient};
-    console.log(query)
-    const bookings = await bookingCollection.find(query).toArray();
-    res.send(bookings);
-  })
+    const decodedEmail = req.decoded.email;
+  console.log(patient)
+           if(decodedEmail===patient){
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+           }
+  
+    else {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+  });
+
 
 
 
@@ -96,8 +147,6 @@ finally{
 
 }
 run().catch(console.dir)
-
-
 
 
 app.get('/', (req, res) => {
