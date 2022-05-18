b56b07c5d97a00a2d04ab1efb5bdbbb929cade11
emonahmed123 <imonshomon@gmail.com>
2022-05-17 20:50:01 -0700

    added heroku deploy insure

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function verifyJWT(req, res, next) {
   if (!authHeader) {
     return res.status(401).send({ message: 'UnAuthorized access' });
   }
-  console.log(authHeader)
+  // console.log(authHeader)
   const token = authHeader.split(' ')[1];
   
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-   console.log(decoded)
+  //  console.log(decoded)
    
    if (err) {
       return res.status(403).send({ message: 'Forbidden access' })
@@ -52,7 +52,59 @@ try{
         const services = await cursor.toArray()
         res.send(services)
      });
-        
+
+
+    //  all user 
+    app.get('/user',verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+     app.get('/admin/:email',async(req,res)=>{
+             const email =req.params.email
+             const user =await userCollection.findOne({email:email})
+              const isAdmin =user.role ==='admin'
+              res.send({admin:isAdmin})
+
+     })
+
+     
+
+       
+    app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
+      // const email = req.params.email;
+      // const filter = {email: email};
+      //  const updateDoc ={
+      //    $set :{role:'admin'},
+      //  };
+      //  const result =await userCollection.updateOne(filter,updateDoc)
+      //   const token =jwt.sign({email:email}, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      //  res.send({ result ,token});
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+    
+    
+    
+    
+    
+    
+    });
+
+
+
+
         app.put('/user/:email',async(req,res)=>{
           const email = req.params.email;
           const user = req.body
